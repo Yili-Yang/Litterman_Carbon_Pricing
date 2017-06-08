1b83e9a83855c56280b742413c9b02ab00d3c0dc
shuxin1214 <xshu2@u.rochester.edu>
2017-06-08 16:21:45 -0400

    Modified bau.py

--- ezclimate/bau.py
+++ ezclimate/bau.py
@@ -1,6 +1,7 @@
 import numpy as np
 from abc import ABCMeta, abstractmethod
 
+
 class BusinessAsUsual(object):
     """Abstract BAU class for the EZ-Climate model.
 
@@ -10,7 +11,7 @@ class BusinessAsUsual(object):
         today's GHG-level
     ghg_end : float
         GHG-level in the last period
-    
+
     Attributes
     ----------
     ghg_start : float
@@ -18,15 +19,16 @@ class BusinessAsUsual(object):
     ghg_end : float
         GHG-level in the last period
     emission_by_decisions : ndarray
-        emissions at decision time periods??
+        emission levels at decision points
     emission_per_period : ndarray
-        total emission at decision time period??
+        amounts of emission for each single period.
     emission_to_ghg : ndarray
-        GHG levels in decision time period??
+        change in GHGs level attributed to each period
     emission_to_bau : float
-        constant for converting GHG to emission??
+        constant: the last period increase in GHGs level divided by its emission
 
     """
+    
     __metaclass__ = ABCMeta
     def __init__(self, ghg_start, ghg_end):
         self.ghg_start = ghg_start
@@ -54,7 +56,7 @@ class DLWBusinessAsUsual(BusinessAsUsual):
     ghg_end : float
         GHG-level in the last period
     emit_time : ndarray or list
-        time, in years, from now when emissions occurs
+        time, in years, from now when emissions occur
     emit_level : ndarray or list
         emission levels in future times `emit_time`
 
@@ -65,20 +67,23 @@ class DLWBusinessAsUsual(BusinessAsUsual):
     ghg_end : float
         GHG-level in the last period
     emission_by_decisions : ndarray
-        emissions at decision time periods??
+        emission level a  specific decision time
     emission_per_period : ndarray
-        total emission at decision time period
+        the amount of emission for each period/between two decision times
     emission_to_ghg : ndarray
-        GHG levels in decision time period
+        change in GHGs level attributed to each period
     emission_to_bau : float
-        constant for converting GHG to emission
+        constant: the last period increase in GHGs level divided by its emission
     emit_time : ndarray or list
         time, in years, from now when emissions occurs
     emit_level : ndarray or list
         emission levels in future times `emit_time`
 
     """
-    def __init__(self, ghg_start=400.0, ghg_end=1000.0, emit_time=[0, 30, 60], emit_level=[52.0, 70.0, 81.4]):
+
+    #the default emit_time [0, 30, 60] should  work here, but with a tree model with decision times [0, 30, 60] instead
+    #[2015, 2030, ...]
+    def __init__(self, ghg_start=400.0, ghg_end=1000.0, emit_time=[0, 30, 60], emit_level= [52.0, 70.0, 81.4]):
         super(DLWBusinessAsUsual, self).__init__(ghg_start, ghg_end)
         self.emit_time = emit_time
         self.emit_level = emit_level
@@ -88,7 +93,7 @@ class DLWBusinessAsUsual(BusinessAsUsual):
 
         Parameters
         ----------
-        time : int 
+        time : int
             future time period in years
 
         Returns
@@ -101,22 +106,29 @@ class DLWBusinessAsUsual(BusinessAsUsual):
             emissions = self.emit_level[0] + float(time) / (self.emit_time[1] - self.emit_time[0]) \
                         * (self.emit_level[1] - self.emit_level[0])
         elif time < self.emit_time[2]:
-            emissions = self.emit_level[1] + float(time - self.emit_time[1]) / (self.emit_time[2] 
+            emissions = self.emit_level[1] + float(time - self.emit_time[1]) / (self.emit_time[2]
                         - self.emit_time[1]) * (self.emit_level[2] - self.emit_level[1])
         else:
             emissions = self.emit_level[2]
         return emissions
 
     def bau_emissions_setup(self, tree):
-        """Create default business as usual emissions path. The emission rate in each period is 
+        """Create default business as usual emissions path. The emission rate in each period is
         assumed to be the average of the emissions at the beginning and at the end of the period.
-
         Parameters
         ----------
         tree : `TreeModel` object
             provides the tree structure used
-            
+
         """
+        num_periods = tree.num_periods
+        self.emission_by_decisions = np.zeros(num_periods)
+        self.emission_per_period = np.zeros(num_periods)
+        self.bau_path = np.zeros(num_periods)
+        self.bau_path[0] = self.ghg_start
+        self.emission_by_decisions[0] = self.emission_by_time(tree.decision_times[0])
+        period_len = tree.decision_times[1:] - tree.decision_times[:-1]
+
         num_periods = tree.num_periods
         self.emission_by_decisions = np.zeros(num_periods)
         self.emission_per_period = np.zeros(num_periods)
@@ -128,10 +140,13 @@ class DLWBusinessAsUsual(BusinessAsUsual):
         for n in range(1, num_periods):
             self.emission_by_decisions[n] = self.emission_by_time(tree.decision_times[n])
             self.emission_per_period[n] = period_len[n] * (self.emission_by_decisions[n-1:n].mean())
+            #the average is the average of the emission level at the beginning and the end of the period.
 
-        #the total increase in ghg level of 600 (from 400 to 1000) in the bau path is allocated over time
+       #the total increase in ghg level of 600 (from 400 to 1000) in the bau path is allocated over time
         self.emission_to_ghg = (self.ghg_end - self.ghg_start) * self.emission_per_period / self.emission_per_period.sum()
+        #emission_to_bau is essentially the last period ghg level divided by the emission level.
         self.emission_to_bau = self.emission_to_ghg[-1] / self.emission_per_period[-1]
+        #find the bau_path based on the last period's emission_to_bau
         for n in range(1, num_periods):
             self.bau_path[n] = self.bau_path[n-1] + self.emission_per_period[n]*self.emission_to_bau
 
