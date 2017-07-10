0609cb91bf7d3f37eda285251ddddd13ef6dd375
Yili Yang <yili.yang@columbia.edu>
2017-07-10 16:22:38 -0400

    intergration

--- ezclimate_matlab/Quasi_Newton_intergration.m
+++ ezclimate_matlab/Quasi_Newton_intergration.m
@@ -1,4 +1,4 @@
-m_0 = py.Matlabmod_g.get_py_m(zeros(1,63);
+m_0 = py.Matlabmod_g.get_py_m(zeros(1,63));
 m_in_mat_0 = double(py.array.array('d',m_0));
 fun = @(x)matlab_utility_g(x);
 [fmin2,xmin2,fcount2] = quasi_newton(fun,m_in_mat_0)
