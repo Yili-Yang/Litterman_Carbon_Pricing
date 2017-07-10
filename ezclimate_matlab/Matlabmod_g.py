3139748ade1eb8c2baeebe8b57d37147e47c85fb
Yili-Yang <yili.yang@columbia.edu>
2017-07-10 17:18:25 -0400

    intergration

--- ezclimate_matlab/Matlabmod_g.py
+++ ezclimate_matlab/Matlabmod_g.py
@@ -30,7 +30,7 @@ def get_py_m(m):
         grad = gs_model.numerical_gradient(m)
         return u.utility(m),grad
     else :
-        ga_model = GeneticAlgorithm(pop_amount=150, num_generations=75, cx_prob=0.8, mut_prob=0.5, 
+        ga_model = GeneticAlgorithm(pop_amount=150, num_generations=1, cx_prob=0.8, mut_prob=0.5, 
                               bound=1.5, num_feature=63, utility=u, print_progress=True) 
         final_pop, fitness = ga_model.run()
         sort_pop = final_pop[np.argsort(fitness)][-1]
--- ezclimate_matlab/matlab_utility_g.m
+++ ezclimate_matlab/matlab_utility_g.m
@@ -1,5 +1,6 @@
 function [re_array,g] = matlab_utility_g(m_in_mat)
-pytuple = py.Matlabmod_g.get_py_m(m_in_mat);
+m_in_py = py.array.array(m_in_mat);
+pytuple = py.Matlabmod_g.get_py_m(m_in_py);
 re_array = double(py.array.array('d',pytuple(1)));
 g = double(py.array.array('d',py.numpy.nditer(pytuple(2))))';
 end
