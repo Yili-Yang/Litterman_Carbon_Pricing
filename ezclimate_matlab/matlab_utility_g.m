dd376b25b39af5192abb9aecb67503beadfba539
Yili-Yang <yili.yang@columbia.edu>
2017-07-10 17:58:41 -0400

    1

--- ezclimate_matlab/matlab_utility_g.m
+++ ezclimate_matlab/matlab_utility_g.m
@@ -1,5 +1,5 @@
 function [re_array,g] = matlab_utility_g(m_in_mat)
-m_in_mat = m_in_mat'
+m_in_mat = m_in_mat';
 m_in_mat = reshape(m_in_mat,[1,numel(m_in_mat)]);
 pytuple = py.Matlabmod_g.get_py_m(m_in_mat);
 re_array = double(py.array.array('d',pytuple(1)));
