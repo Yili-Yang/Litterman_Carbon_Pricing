99cac62d55fd5ac0a19a70717a62ccbf058edc2e
Yili Yang <yili.yang@columbia.edu>
2017-07-10 16:21:32 -0400

    intergration

--- ezclimate_matlab/Matlabmod_g.py
+++ ezclimate_matlab/Matlabmod_g.py
@@ -24,7 +24,7 @@ def get_py_m(m):
     df.damage_simulation(draws=4000000, peak_temp=6.0, disaster_tail=18.0, tip_on=True, 
                              temp_map=0, temp_dist_params=None, maxh=100.0)
     u = EZUtility(tree=t, damage=df, cost=c, period_len=5.0, eis=0.9, ra=7.0, time_pref=0.005)
-    ga_model = GeneticAlgorithm(pop_amount=150, num_generations=1, cx_prob=0.8, mut_prob=0.5, 
+    ga_model = GeneticAlgorithm(pop_amount=150, num_generations=75, cx_prob=0.8, mut_prob=0.5, 
                               bound=1.5, num_feature=63, utility=u, print_progress=True) 
     final_pop, fitness = ga_model.run()
     sort_pop = final_pop[np.argsort(fitness)][-1]
