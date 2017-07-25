ea927e489c6c99f78164af7e16010b6c6aa551e4
Yili-Yang <yili.yang@columbia.edu>
2017-07-25 13:04:24 -0400

    GA_fmincon

--- ezclimate/GA_Fmincon.m
+++ ezclimate/GA_Fmincon.m
@@ -1,3 +1,5 @@
+for count = 1:3
+profile on
 multiprocessing_setup() % set up multiprocessing package, manully call the exectuable of python
 varargin = py.Matlabmod.matlabmode(); % init the class in Matlabmode_g
 pytuple_GA = py.Matlabmod.get_start(varargin);% call GA in matlabmode_g to get a start point for the local optimizer
@@ -5,5 +7,8 @@ m_in_mat_0 = double(py.array.array('d',py.numpy.nditer(pytuple_GA(2))))'; % chan
 [utlity_GA,g_GA] = matlab_utility_g_multiprocessing(m_in_mat_0,varargin);% get the utilty and gradient after GA
 norm_g_GA = norm(g_GA);% get the norm of gradient after GA
 f_fun = @(x)matlab_utility_g(x,varargin);
-[x,fval,exitflag,output,lambda,grad,hessian]= fmincon(f_fun,m_in_mat_0,eye(63,63),ones(1,63));% run Quasi_Newton loacl optimizer
-save('GA_fmincon')
+[x,fval,exitflag,output,lambda,grad,hessian]= fmincon(f_fun,m_in_mat_0,eye(63,63),2*ones(1,63));% run Quasi_Newton loacl optimizer
+save(['GA_fmincon_','',num2str(count)])
+profile off
+profsave(profile('info'),['GA_fmincon_profile_','',num2str(count)])
+end
