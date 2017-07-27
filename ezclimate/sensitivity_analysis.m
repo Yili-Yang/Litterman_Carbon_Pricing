c1d01667e2609d507e98163e81517ea5a6eaf85d
Yili-Yang <yili.yang@columbia.edu>
2017-07-27 09:49:51 -0400

    version 1.4

---
+++ ezclimate/sensitivity_analysis.m
@@ -0,0 +1,9 @@
+multiprocessing_setup() % set up multiprocessing package, manully call the exectuable of python
+varargin = py.Matlabmod.matlabmode(1); % init the class in Matlabmode_g
+m_in_mat_0 = rand(1,63)';
+fun = @matlab_utility_g_multiprocessing;
+[fmin2,xmin2,fcount2,~,iter] = Quasi_Newton(fun,m_in_mat_0,varargin);% run Quasi_Newton loacl optimizer
+[~,fg] = fun(xmin2,varargin);
+final_norm_g_QN = norm(fg);
+price = -double(py.array.array('d',py.numpy.nditer(py.Matlabmod.get_price(xmin2',varargin))))';
+parameters = -double(py.array.array('d',py.numpy.nditer(py.Matlabmod.get_parameters(varargin))))';
