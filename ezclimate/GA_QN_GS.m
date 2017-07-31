29faa9087fcf67475b7ad5d2ce31b33bffc0bac4
shuxin1214 <xshu2@u.rochester.edu>
2017-07-31 09:54:10 -0400

    update

--- ezclimate/GA_QN_GS.m
+++ ezclimate/GA_QN_GS.m
@@ -50,3 +50,58 @@ profile off
 save(['GA_GS_QN_','',num2str(count)])
 profsave(profile('info'),['GA_GS_QN_profile_','',num2str(count)])
 end
+
+file_name = 'GA_GS_QN_';%read the results
+number = 'How manys files need to be read?';
+number = input(number);
+utility_after_ga = [];
+utility_after_gs = [];
+norm_after_ga = [];
+final_norm_grad_gs = [];
+final_norm_grad_qn = [];
+num_of_utility = [];
+num_of_grad_qn = [];
+num_of_iter = [];
+final_value = [];
+m_qn = [];
+
+for x = 1:number
+    load([file_name, '', num2str(x), '', '.mat']);
+    utility_after_ga(end+1) = utlity_GA;
+    utility_after_gs(end+1) = utility_gs;
+    norm_after_ga(end+1) = norm_g_GA;
+    final_norm_grad_gs(end+1) = final_norm_g_GS;
+    final_norm_grad_qn(end+1) = final_norm_g_QN;
+    num_of_utility(end+1) = fcount2;
+    num_of_grad_qn(end+1) = fcount2;
+    num_of_iter(end+1) = iter;
+    final_value(end+1) = fmin2;
+    m_qn = [m_qn, xmin2];
+end
+
+ m_qn=[m_qn, mean(m_qn,2),std(m_qn,1,2)];
+ results = table;
+ results.No = [1:number]';
+ results.utility_after_ga = utility_after_ga';
+ results.utility_after_gs = utility_after_gs';
+ results.norm_after_ga = norm_after_ga';
+ results.final_norm_grad_gs = final_norm_grad_gs';
+ results.final_norm_grad_qn = final_norm_grad_qn';
+ results.num_of_iter = num_of_iter';
+ results.num_of_utility = num_of_utility';
+ results.num_of_grad_qn = num_of_grad_qn';
+ results.final_value = final_value';
+ 
+ error_init = std(final_value);
+ error_gs= std(utility_after_gs);
+ utility_after_ga = mean(utility_after_ga);
+ utility_after_gs = mean(utility_after_gs);
+ norm_after_ga = mean(norm_after_ga);
+ final_norm_grad_gs = mean(final_norm_grad_gs);
+ final_norm_grad_qn = mean(final_norm_grad_qn);
+ num_of_utility = mean(num_of_utility);
+ num_of_grad_qn = mean(num_of_grad_qn);
+ num_of_iter = mean(num_of_iter);
+ final_value = mean(final_value);
+ 
+ save(['results_','',file_name,'','.mat']);
