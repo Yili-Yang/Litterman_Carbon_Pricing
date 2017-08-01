04a39a6fdb03d4904258a2fb8b3ef1d8b35f01b9
Yili-Yang <yili.yang@columbia.edu>
2017-08-01 08:49:50 -0400

    version 1.4

--- ezclimate/run_sensitivity_analysis.m
+++ ezclimate/run_sensitivity_analysis.m
@@ -1,4 +1,4 @@
-ind = 9;
+ind = 0;
 fmin2_m = [];
 xmin2_m = [];
 fcount2_m = [];
@@ -7,7 +7,7 @@ final_norm_g_QN_m = [];
 price_m = [];
 parameters_m = [];
 profile on 
-for count = 1:6
+for count = 1:15
     [fmin2,xmin2,fcount2,iter,final_norm_g_QN,price,parameters] = sensitivity_analysis(ind);
     fmin2_m = [fmin2_m;fmin2];
     xmin2_m = [xmin2_m,xmin2];
@@ -18,5 +18,5 @@ for count = 1:6
     parameters_m =[parameters_m,parameters];
 end
 profile off
-save('sensitivity_base/sensitivity_base_6')
-profsave(profile('info'),'sensitivity_base/sensitivity_base_6_profiler')
+save('sensitivity_base/sensitivity_1_15')
+profsave(profile('info'),'sensitivity_base/sensitivity_1_15_profiler')
