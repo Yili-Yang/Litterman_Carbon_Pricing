87fcf5060ae3d59c1a73e2b782c699ee8bdb1ce4
shuxin1214 <xshu2@u.rochester.edu>
2017-08-08 15:06:22 -0400

    hessian

--- ezclimate/optimal_hessian.m
+++ ezclimate/optimal_hessian.m
@@ -2,16 +2,16 @@ file = input('What is the name of the file? ','s');
 sample = input('How many samples should we test? ');
 name = input('What is the name of the optimal mitigation? ');
 load(file);
+timer = [];
+%multiprocessing_setup();
+varargin = py.Matlabmod.matlabmode(-1);
 for j = 1:sample
     m_optimal = name(:,sample);
-    %multiprocessing_setup();
-    varargin = py.Matlabmod.matlabmode(-1);
     myfun = @matlab_utility;
-    timer = [];
     profile on
     hessian = NumHessian(myfun,m_optimal,varargin); 
     sym = (hessian+hessian')/2;
-    [~,num]=cholcov(sym,0);
+    [~,num]=cholcov(sym);
     display(num);
     if num ~= 0
          disp(['Sample','',num2str(j),'',' is not positive definite.']);
