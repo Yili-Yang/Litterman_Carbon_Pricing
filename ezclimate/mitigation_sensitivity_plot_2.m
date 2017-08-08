d3ae425f103f73e9fbc7ce8a5c9b7279c0f775a9
shuxin1214 <xshu2@u.rochester.edu>
2017-08-08 17:04:53 -0400

    plot + report

--- ezclimate/mitigation_sensitivity_plot_2.m
+++ ezclimate/mitigation_sensitivity_plot_2.m
@@ -1,9 +1,11 @@
+%ask for the file name, mitigation variable, and nodes to change
 file = input('What is the name of the file? ', 's');
 load(file);
 name = input('What is the name of the mitigation variable? ');
 x_change = input('Which 2 x are to be changed? ');
 x_change_1 = x_change(1);
 x_change_2 = x_change(2);
+%set up the scale of change
 step = input('What is the stepsize? ');
 count = input('How many steps would you like? ');
 m = name(:,1);
@@ -13,6 +15,8 @@ u_summary = zeros(2*count+1);
 %multiprocessing_setup();
 varargin = py.Matlabmod.matlabmode(-1);
 timer = [];
+%for each change in dimension 1, generate the mitigations under change in
+%dimension 2 and calculate corresponding utilities.
 for i = 1:2*count+1
     profile on
     m_temp = repmat(m,1,2*count+1);
@@ -27,6 +31,7 @@ for i = 1:2*count+1
     timer = [timer;time.FunctionTable];
 end
 save(['Utilities with Mitigation Changes at Node ','',num2str(x_change_1),'',' and Node ','',num2str(x_change_2)]);
+%generate 3-d picture
 figure
 fig = surf(m_change_1,m_change_2,u_summary);
 title(['Utilities with Mitigation Changes at Node ','',num2str(x_change_1),'',' and Node ','',num2str(x_change_2)]);
@@ -34,6 +39,7 @@ xlabel(['Mitigation Level at Node ','',num2str(x_change_1)]);
 ylabel(['Mitigation Level at Node ','',num2str(x_change_2)]);
 zlabel('Utility');
 hold on
+%mark the original point
 scatter3(m_change_1(count+1),m_change_2(count+1),u_summary(count+1,count+1),'filled','y');
 saveas(fig,['Utilities with Mitigation Changes at Node ','',num2str(x_change_1),'',' and Node ','',num2str(x_change_2)],'epsc')
 hold off
