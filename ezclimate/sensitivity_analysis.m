eb94b13ea3edfa9c4112036ad5d49fe46724e944
Yili-Yang <yili.yang@columbia.edu>
2017-08-14 17:13:02 -0400

    version 1.5

--- ezclimate/sensitivity_analysis.m
+++ ezclimate/sensitivity_analysis.m
@@ -3,22 +3,29 @@
 %           Yili Yang, July 2017
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Using Genetic Algorithm to find the start point and then us Quasi-Newton
-% method as the local optimizer to get the optimal mitigation level on each
-% node
+% Change the parameters in damage function and cost function and use random
+% start point and Quasi-Newton method to find the local optimal point.
 %
 % Input:
-% m_in_mat: mitigation level stored in matlab double
-% instance of matlabmode class in Matlabmod (Provide damage simualtion and utility)
-%
+% ind: the indicator of what parameters is been changed.
+%   1. = -1: use original parameters and fixed seed to simulate damage 
+%   2. = 0~8: change the parameters in pindyck damage simulation, 0 is to 
+%             change alpha450, 1 is to change alpha650 ... 8 is to change
+%             theta1000
+%   3. = 9: use orignal parameters and random seed to simulate damage
+%   4. = 10 or 11: change the parameters in cost function, 10 is to change
+%                  x60 and 11 is to change x100
 % Output:
-% re - objective function value: 
-%     1. utility + positive modification term if utility is calculated by python
-%     2. positive modification term if utility is not calculate by python 
-% g - gradient of the utility 
+% fmin2: final optimal object value (utility + positive add up)
+% xmin2: final optimal mititgation point
+% fcount2: function evaluation in this optimization process
+% iter: iteration number of the optimization process
+% final_norm_g_QN: the norm of gradient at the final optimal point.
+% price: the optimal price (SCC) achieved by the final mitigation level.
+% utility_at_each_node: final optimal utiltiy at each node.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [fmin2,xmin2,fcount2,iter,final_norm_g_QN,price,parameters,utlity_at_each_node] = sensitivity_analysis(ind)
-multiprocessing_setup() % set up multiprocessing package, manully call the exectuable of python
+
 varargin = py.Matlabmod.matlabmode(ind); % init the class in Matlabmode_g
 %%%have to make sure it is optimial.
 m_in_mat_0 = rand(1,63)';
