916d930f863ce3b34b60c83bd2dd3c9ef06bdb85
Yili-Yang <yili.yang@columbia.edu>
2017-08-24 11:51:31 -0400

    version 1.5

--- ezclimate/graph_sensitivity_analysis.m
+++ ezclimate/graph_sensitivity_analysis.m
@@ -1,5 +1,18 @@
 function graph_sensitivity_analysis(result_number)
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%           Yili Yang, Aug 2017
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Draw the graphs for sensitivity analysis
+%
+% Input:
+% result_number - index of the sensitivity result that you want to draw the
+% graph
+%
+% Output:
+% a png file in sensitivity_analysis_results folder
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 load(['sensitivity_analysis_results\sensitivity_result_','',num2str(result_number),'','.mat'])
 load('sensitivity_analysis_results\sensitivity_base_results.mat')
 name_cell = {'alpha450', 'alpha650', 'alpha1000', 'beta450', 'beta650', 'beta1000', 'theta450', 'theta650', 'theta1000', 'nothing is changed', 'x60','x100'};
@@ -18,9 +31,12 @@ end
 price_base = -mean(period_price_ave_base,2);
 bound = -prctile(period_price_ave,[10,90],2);
 decision_time = [0, 15, 45, 85, 185, 285]';
+%
+figure_m = figure;
 plot(decision_time,price_base,'b',decision_time,bound,'r--')
 
-title(['Sensitivity Analysis for parameter',' ',name_cell{result_number}])
-xlabel('period')
-ylabel('price in $')
+title(['Sensitivity Analysis for Parameter',' ',name_cell{result_number}])
+xlabel('Period')
+ylabel('Price in $')
+saveas(figure_m,['sensitivity_analysis_results\Sensitivity Analysis for Parameter',' ',name_cell{result_number},'.png'])
 end
