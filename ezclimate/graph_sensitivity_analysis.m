948ffbebfb274b464b6819f1f5bb8a3e7038ad87
Yili-Yang <yili.yang@columbia.edu>
2017-08-24 11:54:22 -0400

    version 1.5

--- ezclimate/graph_sensitivity_analysis.m
+++ ezclimate/graph_sensitivity_analysis.m
@@ -13,9 +13,14 @@ function graph_sensitivity_analysis(result_number)
 % Output:
 % a png file in sensitivity_analysis_results folder
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% load the sensitivity results
 load(['sensitivity_analysis_results\sensitivity_result_','',num2str(result_number),'','.mat'])
 load('sensitivity_analysis_results\sensitivity_base_results.mat')
 name_cell = {'alpha450', 'alpha650', 'alpha1000', 'beta450', 'beta650', 'beta1000', 'theta450', 'theta650', 'theta1000', 'nothing is changed', 'x60','x100'};
+
+% Take the average price of each node within one period to draw the graph
+% Since each node within one period has the same probability, the expected
+% value is the average.
 period_price_ave = [];
 for i =1:6
     start_i = 2^(i-1);
@@ -29,9 +34,10 @@ for i =1:6
     period_price_ave_base = [period_price_ave_base;mean(price_m_t(start_i:end_i,:),1)];
 end
 price_base = -mean(period_price_ave_base,2);
-bound = -prctile(period_price_ave,[10,90],2);
+% take 90% quantile
+bound = -prctile(period_price_ave,[5,95],2);
 decision_time = [0, 15, 45, 85, 185, 285]';
-%
+% plot the figure
 figure_m = figure;
 plot(decision_time,price_base,'b',decision_time,bound,'r--')
 
